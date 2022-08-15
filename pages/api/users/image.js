cbf2af6ec404594fe1ad6ca82366d2c61f5793c3
Yusra <yusraa190@gmail.com>
2022-08-16 02:26:26 +0300

    fix deployment issues 5

--- pages/api/users/image.js
+++ pages/api/users/image.js
@@ -14,7 +14,7 @@ const __dirname = path.resolve();
 
 let storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, path.join(__dirname, "public/uploads"));
+    callback(null, path.join(__dirname, "/tmp"));
   },
   filename: (req, file, callback) => {
     callback(null, Date.now() + "--" + file.originalname);
